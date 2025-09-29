3236a2c79b7f3b39b7463c34a7050fabded088ad
Muhammad Ashique <ashiquebinraheem@gmail.com>
2025-09-29 19:14:09 +0530

    Approve request

--- Louvre/Pages/Approver/ApproveRequest.cshtml.cs
+++ Louvre/Pages/Approver/ApproveRequest.cshtml.cs
@@ -241,6 +241,391 @@ namespace Louvre.Pages
         }
         #endregion
 
+
+        public async Task<IActionResult> OnPostApproveAsync()
+        {
+            BaseResponse result = new BaseResponse();
+            try
+            {
+                RequestApproval.ApprovalStage = await GetCurrentApprovalStage();
+
+                var pendingApprovalCount = await _dbContext.GetAsync<int>($@"SELECT  Count(S.Stage)
+                FROM viRequest R
+                JOIN RequestTypeApprovalStage S on S.RequestTypeID = R.RequestTypeID and S.Stage>@Stage
+                JOIN UserTypes U on U.UserTypeID=S.UserTypeID
+                Where R.RequestID=@RequestID and Case when U.UserNature=2 and R.IsDisposalRequired=0 then 0 else 1 end=1",
+                new { RequestID = RequestApproval.RequestID, Stage = RequestApproval.ApprovalStage });
+
+                if (pendingApprovalCount > 0)
+                    RequestApproval.NeedHigherLevelApproval = true;
+
+                await _dbContext.SaveAsync(RequestApproval);
+
+                if (pendingApprovalCount == 0)
+                {
+                    #region  Send Gatepass
+
+                    var gatepassDatas = (await _dbContext.GetEnumerableAsync<GatePassViewModel>($@"Select R.RequestID,D.RequestVehicleID,E.QRCode,R.CompanyName,R.EmployeeName,DesignationName,
+			        E.ContactNumber,PlateNo,VT.VehicleTypeName,VehicleSize,RegisterNo, Slot, LocationName,Coalesce(RQ.Email,P.Email1) as Email,
+					ISNULL(B.GoogleLocation,'') as GoogleLocation
+			        From viRequest R
+					JOIN Employee RQ on RQ.EmployeeID=R.EmployeeID
+			        JOIN RequestVehicle D on D.RequestID=R.RequestID
+			        JOIN Vehicle V on V.VehicleID=D.VehicleID
+			        JOIN Employee E on E.EmployeeID=D.EmployeeID
+			        LEFT JOIN EmployeeDesignation ED on ED.DesignationID=E.DesignationID
+			        LEFT JOIN VehicleType VT on VT.VehicleTypeID=V.VehicleTypeID
+					LEFT JOIN viPersonalInfos P on P.UserID=R.RequestedByID
+                    LEFT JOIN Branch B on B.BranchID=R.SubBranchID
+                    Where R.RequestID=@RequestID", new { RequestID = RequestApproval.RequestID })).ToList();
+
+
+                    var url = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+
+                    var body = $@"<!DOCTYPE html>
+					<html>
+					<head>
+					<title>Louvre - Gate Pass</title>
+					<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap' rel='stylesheet'>
+					</head>
+					<body style='padding: 0;margin: 0; font-family: 'Open Sans', sans-serif;'>
+
+					<div style='width:600px;height:auto;background-color:#f5f5f5;margin: 0 auto;margin-top:50px;-webkit-box-shadow: 0px 0px 20px rgba(0,0,0,0.1);-moz-box-shadow: 0px 0px 20px rgba(0,0,0,0.1);-o-box-shadow: 0px 0px 20px rgba(0,0,0,0.1);box-shadow: 0px 0px 20px rgba(0,0,0,0.1);border-radius: 10px;padding: 30px;'>
+					
+					<div style='width:100%;height:80px;background-color:#e4edf2;' align='center'>
+						<img src='{url}/assets/img/brand.jpg' class='logo' style='width:200px;padding-top: 16px;'>
+					</div>";
+
+                    foreach (var item in gatepassDatas)
+                    {
+
+                        var image = _mediaRepository.GetQRImage(item.QRCode);
+                        var msg = $"<img alt='Embedded Image'  width='200' src =\"{image}\">";
+
+                        body += $@"
+						<div style='background-color:none;width:100%;float: left;margin-top:10px;'>
+						<center>{msg}</center>
+						</div>
+
+                        <div style='background-color:none;width:100%;float: left;'>
+							<p style='color: #666;padding-top: 0px; font-size: 14px;'><center>{item.QRCode}</center></p>
+						</div>
+
+                        <div style='background-color:none;width:50%;float: left;'>
+						    <p style='color: #666;padding-left: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Slot:</span> {item.Slot}</p>
+					    </div>
+    
+					    <div style='background-color:none;width:50%;float: left;'>
+						    <p style='color: #666;padding-top: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Location:</span> {item.LocationName}</p>
+					    </div>
+    
+						<div style='background-color:none;width:50%;float: left;'>
+						<p style='color: #666;padding-left: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Company Name:</span> {item.CompanyName}</p>
+						</div>
+    
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;padding-top: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Name:</span> {item.EmployeeName}</p>
+						</div>
+    
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;padding-left: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Mobile No:</span> {item.ContactNumber}</p>
+						</div>
+    
+						<div style='background-color:none;float: left;'>
+							<p style='color: #666;padding-top: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Designation:</span> {item.DesignationName}</p>
+						</div>
+    
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;font-size: 14px;'><span style='font-weight: 600;color:#333;'>Register No:</span> {item.RegisterNo}</p>
+						</div>
+    
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;padding-left:0px;font-size: 14px;'><span style='font-weight: 600;color:#333;'>Vehicle Size:</span> {item.VehicleSize}</p>
+						</div>
+
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;padding-left:0px;font-size: 14px;'><span style='font-weight: 600;color:#333;'>Plate No:</span> {item.PlateNo}</p>
+						</div>
+
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;padding-left:0px;font-size: 14px;'><span style='font-weight: 600;color:#333;'>Type of Vehicle:</span> {item.VehicleTypeName}</p>
+						</div>";
+                    }
+
+                    Passengers = await _commonRepository.GetPassengersAsync(RequestApproval.RequestID.Value);
+
+                    if (Passengers.Count > 0)
+                    {
+
+                        body += $@"<table cellpadding='5' cellspacing='0' style='border: 1px solid #ccc;font-size: 9pt;margin: 10px 0px;width: 100%;'>
+									<tr>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Co-Passenger</th>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Designation</th>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Company</th>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Mobile No</th>
+									</tr>";
+
+                        foreach (var member in Passengers)
+                        {
+                            body += $@"<tr>
+								<td style='border: 1px solid #ccc;'>{member.EmployeeName}</td>
+								<td style='border: 1px solid #ccc;'>{member.DesignationName}</td>
+								<td style='border: 1px solid #ccc;'>{member.CompanyName}</td>
+                                <td style='border: 1px solid #ccc;'>{member.ContactNumber}</td>
+							</tr>";
+                        }
+                        body += "</table>";
+
+                    }
+
+                    var meterials = await _commonRepository.GetMeterialsAsync(RequestApproval.RequestID.Value);
+
+                    if (meterials.Count > 0)
+                    {
+
+                        body += $@"<center><table cellpadding='5' cellspacing='0' style='border: 1px solid #ccc;font-size: 9pt;margin: 10px 10px;width: 100%;'>
+									<tr>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>SlNo</th>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Meterial</th>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Quantity</th>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Unit</th>
+									</tr>";
+
+                        for (int i = 0, j = 1; i < meterials.Count(); i++, j++)
+                        {
+                            body += $@"<tr>
+								<td style='border: 1px solid #ccc;'>{i + 1}</td>
+								<td style='border: 1px solid #ccc;'>{meterials[i].Description}</td>
+								<td style='border: 1px solid #ccc;'>{meterials[i].Quantity}</td>
+								<td style='border: 1px solid #ccc;'>{meterials[i].PackingTypeName}</td>
+							</tr>";
+                        }
+                        body += "</table></center>";
+
+                    }
+
+                    body += $@"
+                          <div style='width:auto;height:80px;margin-top:10px;' align='center'>
+                            <a href='{gatepassDatas[0].GoogleLocation}'>
+						    <img src='{url}/map.png' class='logo' style='padding-top: 16px;width:100%'>
+                            </a>
+					    </div>  
+					</html>";
+
+
+                    body += $@"
+                        <div style='width:auto;height:80px;margin-top:10px;' align='center'>
+						    <img src='{url}/assets/img/full-brand.jpg' class='logo' style='padding-top: 16px;width:100%'>
+					    </div>
+                        </div></body>    
+					</html>";
+
+                    await _emailSender.SendEmailAsync(gatepassDatas[0].Email, "Gatepass", body);
+
+                    #endregion
+
+                    var request = await _dbContext.GetAsync<Request>(RequestApproval.RequestID.Value);
+
+                    #region Send email to host
+
+                    //if (!string.IsNullOrEmpty(request.HostEmail))
+                    //{
+                    //    await _emailSender.SendHtmlEmailAsync(request.HostEmail, "Request Approved", body);
+                    //}
+
+                    #endregion
+
+                    #region Explosive Item Mail
+
+                    if (request.ContainsExplosive)
+                    {
+
+                        body = "";
+
+
+                        foreach (var item in gatepassDatas)
+                        {
+                            body += $@"
+
+                        <div style='background-color:none;width:100%;float: left;'>
+						    <p style='color: #666;padding-left: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Barcode:</span> {item.QRCode}</p>
+					    </div>
+
+                        <div style='background-color:none;width:50%;float: left;'>
+						    <p style='color: #666;padding-left: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Slot:</span> {item.Slot}</p>
+					    </div>
+    
+					    <div style='background-color:none;width:50%;float: left;'>
+						    <p style='color: #666;padding-top: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Location:</span> {item.LocationName}</p>
+					    </div>
+    
+						<div style='background-color:none;width:50%;float: left;'>
+						<p style='color: #666;padding-left: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Company Name:</span> {item.CompanyName}</p>
+						</div>
+    
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;padding-top: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Name:</span> {item.EmployeeName}</p>
+						</div>
+    
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;padding-left: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Mobile No:</span> {item.ContactNumber}</p>
+						</div>
+    
+						<div style='background-color:none;float: left;'>
+							<p style='color: #666;padding-top: 0px; font-size: 14px;'><span style='font-weight: 600;color:#333;'>Designation:</span> {item.DesignationName}</p>
+						</div>
+    
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;font-size: 14px;'><span style='font-weight: 600;color:#333;'>Register No:</span> {item.RegisterNo}</p>
+						</div>
+    
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;padding-left:0px;font-size: 14px;'><span style='font-weight: 600;color:#333;'>Vehicle Size:</span> {item.VehicleSize}</p>
+						</div>
+
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;padding-left:0px;font-size: 14px;'><span style='font-weight: 600;color:#333;'>Plate No:</span> {item.PlateNo}</p>
+						</div>
+
+						<div style='background-color:none;width:50%;float: left;'>
+							<p style='color: #666;padding-left:0px;font-size: 14px;'><span style='font-weight: 600;color:#333;'>Type of Vehicle:</span> {item.VehicleTypeName}</p>
+						</div>";
+                        }
+
+                        body += $@"<center><table cellpadding='5' cellspacing='0' style='border: 1px solid #ccc;font-size: 9pt;margin: 10px 10px;width: 80%;'>
+									<tr>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>SlNo</th>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Description</th>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Quantity</th>
+									<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Unit</th>
+									</tr>";
+
+                        for (int i = 0, j = 1; i < meterials.Count(); i++, j++)
+                        {
+                            body += $@"<tr>
+								<td style='border: 1px solid #ccc;'>{i + 1}</td>
+								<td style='border: 1px solid #ccc;'>{meterials[i].Description}</td>
+								<td style='border: 1px solid #ccc;'>{meterials[i].Quantity}</td>
+								<td style='border: 1px solid #ccc;'>{meterials[i].PackingTypeName}</td>
+							</tr>";
+                        }
+                        body += "</table></center>";
+
+                        var mailSettings = await _dbContext.GetAsync<MailSettings>(1);
+                        await _emailSender.SendHtmlEmailAsync(mailSettings.MailTo, "Explosive Content", body);
+                    }
+
+                    #endregion
+
+                    result.CreatSuccessResponse(102);
+                }
+                else
+                {
+                    #region Mail to appprovers
+
+                    var url = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+                    await _commonRepository.SendNewRequestMailToApprover(RequestApproval.RequestID.Value, url);
+
+                    #endregion
+
+                    result.CreatSuccessResponse(103);
+                }
+
+            }
+            catch (Exception err)
+            {
+                result = await _errorLogRepo.CreatThrowResponse(err.Message, CurrentUserID);
+            }
+            return new JsonResult(result);
+        }
+
+        public async Task<IActionResult> OnPostConfirmAsync()
+        {
+            BaseResponse result = new BaseResponse();
+            try
+            {
+                RequestApproval.ApprovalStage = await GetCurrentApprovalStage();
+                RequestApproval.NeedHigherLevelApproval = true;
+                await _dbContext.SaveAsync(RequestApproval);
+                result.CreatSuccessResponse(103);
+            }
+            catch (Exception err)
+            {
+                result = await _errorLogRepo.CreatThrowResponse(err.Message, CurrentUserID);
+            }
+            return new JsonResult(result);
+        }
+
+        public async Task<IActionResult> OnPostRejectAsync()
+        {
+            BaseResponse result = new BaseResponse();
+            try
+            {
+                RequestApproval.ApprovalStage = await GetCurrentApprovalStage();
+                RequestApproval.IsRejected = true;
+                await _dbContext.SaveAsync(RequestApproval);
+                result.CreatSuccessResponse(104);
+
+
+                #region  Send Reject Mail
+
+                var data = await _dbContext.GetAsync<RequestRejectMailModel>($@"Select Coalesce(RQ.Email,P.Email1) as Email,RequestNo,Remarks,SLot
+			        From viRequest R
+					JOIN Employee RQ on RQ.EmployeeID=R.EmployeeID
+					LEFT JOIN viPersonalInfos P on P.UserID=R.RequestedByID
+                    Where R.RequestID=@RequestID", new { RequestID = RequestApproval.RequestID });
+
+
+                var url = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+
+                var body = $@"<!DOCTYPE html>
+					<html>
+					<head>
+					<title>Louvre - Request Rejected</title>
+					<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap' rel='stylesheet'>
+					</head>
+					<body style='padding: 0;margin: 0; font-family: 'Open Sans', sans-serif;'>
+
+					<div style='width:600px;height:auto;background-color:#f5f5f5;margin: 0 auto;margin-top:50px;-webkit-box-shadow: 0px 0px 20px rgba(0,0,0,0.1);-moz-box-shadow: 0px 0px 20px rgba(0,0,0,0.1);-o-box-shadow: 0px 0px 20px rgba(0,0,0,0.1);box-shadow: 0px 0px 20px rgba(0,0,0,0.1);border-radius: 10px;padding: 30px;'>
+					
+					<div style='width:100%;height:80px;background-color:#e4edf2;' align='center'>
+						<img src='{url}/assets/img/brand.jpg' class='logo' style='width:200px;padding-top: 16px;'>
+					</div>
+
+                    <div style='background-color:none;width:100%;float: left;'>
+						<p style='color: #666;padding-left: 0px; font-size: 14px;'>
+                            Your request for gatepass Request No: {data.RequestNo} on {data.Slot} has been rejected for the following reason.<br/>
+                            {data.Remarks}<br/>
+                            Please resubmit the application.<br/><br/>
+                            Thanks
+                        </p>
+					</div>";
+
+                body += $@"
+                        <div style='width:auto;height:80px;margin-top:10px;' align='center'>
+						    <img src='{url}/assets/img/full-brand.jpg' class='logo' style='padding-top: 16px;width:100%'>
+					    </div>
+                        </div></body>    
+					</html>";
+
+                await _emailSender.SendEmailAsync(data.Email, "Request Rejected", body);
+
+                #endregion
+
+
+            }
+            catch (Exception err)
+            {
+                result = await _errorLogRepo.CreatThrowResponse(err.Message, CurrentUserID);
+            }
+            return new JsonResult(result);
+        }
+
+        public async Task<IActionResult> OnPostLoadSlotAsync()
+        {
+            return new JsonResult(await GetSlots());
+        }
+
         private async Task<IEnumerable<IdnValuePair>> GetSlots()
         {
             return await _dbContext.GetEnumerableAsync<IdnValuePair>(
